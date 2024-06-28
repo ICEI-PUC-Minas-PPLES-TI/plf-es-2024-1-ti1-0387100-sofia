5bda6aa4b505897b4b14e0dcaf612f445f1933bc
Md1o1 <kelvinmendes8@gmail.com>
2024-06-28 20:30:11 -0300

    add templates

--- codigo/SOFIA-teste/assets/JS/pagina-inicial.js
+++
@@ -1,9 +0,0 @@
-//Animacao Inicial
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    // Esconde a tela de carregamento
-    document.getElementById("loader").style.display = "none";
-    // Mostra o conteúdo da página
-    document.getElementById("content").style.display = "block";
-  }, 3000);
-});
